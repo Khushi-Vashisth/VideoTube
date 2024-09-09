6158350b6beb304dd8b2cf5b28e91fd85bf9da50
khushi Vashisth <khushidhiman951@gmail.com>
2024-09-09 12:13:32 +0530

    setup routes-controllers

--- Backend/index.js
+++ Backend/index.js
@@ -13,6 +13,7 @@ app.use(
   })
 );
 
+//MONGODB CONNECTION
 const connectDB = async () => {
   try {
     const connection = await mongoose.connect(process.env.MONGO_URL);
@@ -32,3 +33,13 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.static("public"));
 
 connectDB();
+
+//Import Routes
+import userRouter from "./src/routes/user.route.js";
+
+//Declaration of Routes
+app.use("/api/v1", userRouter);
+
+app.get("/", (req, res) => {
+  res.send("welcome");
+});
